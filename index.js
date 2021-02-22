a488f37cc296c7380f1dc8a598f952b1014e57c0
andreysena <andreysena14@gmail.com>
2021-02-21 23:30:36 -0300

    Concluded: Adjustments made ✅

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-//Carregando o módulo express (parte 1 da aula)
+//Carregando o módulo express
 const express = require('express')
 
 //Carregando o módulo express
 const bodyParser = require('body-parser')
 
-//Criando a variável que recebe a instância do express (parte 1 da aula)
+//Criando a variável que recebe a instância do express
 const app = express()
 
 //Carregando o módulo express-handlebars
@@ -120,40 +120,8 @@ const { sequelize } = require('./models/db')
         })
     })
 
-//Iniciando o servidor com o express e indicando a porta (parte 1 da aula)
+//Tudo relacioando ao express deve ser inserido acima desta linha
+//Iniciando o servidor com o express e indicando a porta
 app.listen(8081, () => (
-console.log('O servidor está rodando em: http://localost:8081')
-)) //tudo relacioando ao express deve ser inserido acima desta linha
-
-
-
-
-
-
-
-
-
-
-
-
-// //Criando rotas para a aplicações (parte 1 da aula)
-// app.get('/', (req, res) =>(
-//     res.sendFile(__dirname + "/pages/index.html")
-// ))
-
-// app.get('/sobre', (req, res) => (
-//     res.sendFile(__dirname + "/pages/sobre.html")
-// ))
-
-// app.get('/blog', (req, res) => (
-//     res.send('Bem Vindo ao meu blog')
-// ))
-
-// //Criando uma rota para dar boas vindas utilizando parâmetros (parte 2 da aula)
-// app.get('/ola/:nome/:sobrenome/:idade', (req, res) => (
-
-//     res.send("<h1>Olá, " + req.params.nome + " " + req.params.sobrenome +"</h1>"+"<h2> Sua idade é: " + req.params.idade + " anos</h2>")
-// ))
-
-
-
+console.log('O servidor está rodando em: http://localhost:8081')
+))
